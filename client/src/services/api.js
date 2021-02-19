a407ef1b7361ef99208088115c13c5740db584fe
JuanGrajales <inbox1.juan+github@gmail.com>
2021-02-19 00:11:50 -0500

    add base url for client api request

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 let baseURL;
 
 process.env.NODE_ENV === "production"
-  ? (baseURL = "/herokuUrl")
+  ? (baseURL = "https://cloudtaskr-v2.herokuapp.com")
   : (baseURL = "http://localhost:5000");
 
 // const service = axios.create({ withCredentials: true, baseURL });
